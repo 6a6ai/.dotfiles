6e8713eeca22465ca16bcae2c2318115359b5d98
6a6ai <6a6ai>
2021-03-10 16:29:57 +0100

    last changes

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.config/nvim/after/plugin
 mkdir -p ~/.config/nvim/lua
 
 # link files
-for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
+for f in `find -E . -regex '.*\.(vim|lua)'`; do
     rm -rf ~/.config/nvim/$f
     ln -s ~/.dotfiles/nvim/$f ~/.config/nvim/$f
 done
